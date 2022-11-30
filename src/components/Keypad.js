b7f98c5d437d5120396137461142e72606d6b32a
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-11-30 11:33:22 +0300

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  return (
+    <div>
+      <input
+        type="password"
+        onChange={() => {
+          console.log("Entering password...");
+        }}
+      />
+    </div>
+  );
 }
 
 export default Keypad;
